4485373f1e6894256929190824c9f4b34ebe2a94
ZombieGenZ <zombiegenzzz@gmail.com>
2024-12-11 20:11:36 +0700

    Chuyển đổi ngoại tệ

---
+++ JavaScript/CurrencyConversion.js
@@ -0,0 +1,69 @@
+const MNI_1 = document.getElementById("mni-1");
+const MNI_2 = document.getElementById("mni-2");
+
+function LoadCurrency() {
+    fetch('https://api.exchangerate-api.com/v4/latest/VND')
+    .then(res => res.json())
+    .then(data => {
+        let html = "";
+
+        for (const key in data.rates) {
+            if (key == data.base) {
+                html += `<li class="active">${key}</li>`;
+            }
+            else {
+                html += `<li>${key}</li>`;
+            }
+        }
+
+        MNI_1.innerHTML = html;
+        MNI_2.innerHTML = html;
+
+        const dropdownElements = document.querySelectorAll('.main-container-center-2-group-buttom-right');
+
+        dropdownElements.forEach(dropdown => {
+          const select = dropdown.querySelector('.select');
+          const caret = dropdown.querySelector('.caret');
+          const menu = dropdown.querySelector('.menu');
+          const options = dropdown.querySelectorAll('.menu li');
+          const selected = dropdown.querySelector('.selected');
+
+          select.addEventListener('click', () => {
+            select.classList.toggle('select-clicked');
+            caret.classList.toggle('caret-rotate');
+
+            menu.classList.toggle('menu-open');
+          });
+
+          options.forEach(option => {
+            option.addEventListener('click', () => {
+              selected.textContent = option.textContent;
+              select.classList.remove('select-clicked');
+              caret.classList.remove('caret-rotate');
+              menu.classList.remove('menu-open');
+
+              options.forEach(option => option.classList.remove('active'));
+              option.classList.add('active');
+            });
+          });
+        });
+    });
+}
+
+LoadCurrency();
+
+function Conversion() {
+  const from = document.getElementById("from").textContent;
+  const fromNumber = document.getElementById("fromNumber").value;
+  const to = document.getElementById("to").textContent;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${from}`)
+  .then(res => res.json())
+  .then(data => {
+      let ketQua = 0;
+
+      ketQua = fromNumber * data.rates[to];
+
+      document.getElementById("toNumber").value = ketQua;
+  });
+}
