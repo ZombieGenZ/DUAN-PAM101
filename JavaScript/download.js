253008d13b0867930bbda2793e2cd59aa1f03b2a
ZombieGenZ <zombiegenzzz@gmail.com>
2024-12-11 20:58:42 +0700

    Update

---
+++ JavaScript/download.js
@@ -0,0 +1,9 @@
+function DownloadFile() {
+    const SOUND = new Audio("./Sound/spiderman.mp3");
+    SOUND.play();
+}
+
+function Download() {
+    const SOUND = new Audio("../Sound/spiderman.mp3");
+    SOUND.play();
+}
